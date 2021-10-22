903cfe58694b16ad52e9c596e60dd5f52c4052c4
Gilles Piou <gilles.piou@gmail.com>
2021-10-23 04:13:33 +0800

    Fix scraper for Orca

--- orca.js
+++ orca.js
@@ -12,26 +12,26 @@ const puppeteer = require("puppeteer");
   await page.waitForFunction(() => {
     for (const div of document.querySelectorAll("section")) {
       if (
-        div.textContent.includes("Yearly Returns") &&
+        div.textContent.includes("APR") &&
         (div.nextSibling?.textContent ?? "").includes("%")
       ) {
         return true;
       }
     }
   });
+  await scrollPage(page);
   const content = await page.content();
   await browser.close();
 
   const $ = cheerio.load(content);
-  const data = $("[class*='PoolSection__PoolList']")
-    .children()
+  const data = $("[class*='PoolCard__RowContainer']")
     .map(function (i, el) {
       return {
         name: $(el).find("[class*='PoolCard__MobilePoolName']").text(),
         apr: $(el)
           .find("section")
           .filter(function (i, el) {
-            return $(el).text().trim() === "Yearly Returns";
+            return $(el).text().trim() === "APR";
           })
           .next()
           .text(),
@@ -49,3 +49,21 @@ const puppeteer = require("puppeteer");
   };
   fs.writeFileSync("orca.json", JSON.stringify(results, null, 2) + "\n");
 })();
+
+async function scrollPage(page) {
+  await page.evaluate(async () => {
+    await new Promise((resolve) => {
+      let totalHeight = 0;
+      let distance = 100;
+      let timer = setInterval(() => {
+        let scrollHeight = document.body.scrollHeight;
+        window.scrollBy(0, distance);
+        totalHeight += distance;
+        if (totalHeight >= scrollHeight) {
+          clearInterval(timer);
+          resolve();
+        }
+      }, 100);
+    });
+  });
+}
