d87931775dbf587e557e1d958e1ac61625e9db8f
Gilles Piou <gilles.piou@gmail.com>
2021-12-26 23:18:16 +0800

    Add scraper for Quarry

--- README.md
+++ README.md
@@ -5,6 +5,7 @@
 | https://pioug.github.io/yield-data/cropper.json   | [![cropper](https://github.com/pioug/yield-data/actions/workflows/cropper.yml/badge.svg)](https://github.com/pioug/yield-data/actions/workflows/cropper.yml)       |
 | https://pioug.github.io/yield-data/mercurial.json | [![mercurial](https://github.com/pioug/yield-data/actions/workflows/mercurial.yml/badge.svg)](https://github.com/pioug/yield-data/actions/workflows/mercurial.yml) |
 | https://pioug.github.io/yield-data/orca.json      | [![orca](https://github.com/pioug/yield-data/actions/workflows/orca.yml/badge.svg)](https://github.com/pioug/yield-data/actions/workflows/orca.yml)                |
+| https://pioug.github.io/yield-data/quarry.json    | [![quarry](https://github.com/pioug/yield-data/actions/workflows/quarry.yml/badge.svg)](https://github.com/pioug/yield-data/actions/workflows/quarry.yml)          |
 | https://pioug.github.io/yield-data/raydium.json   | [![raydium](https://github.com/pioug/yield-data/actions/workflows/raydium.yml/badge.svg)](https://github.com/pioug/yield-data/actions/workflows/raydium.yml)       |
 | https://pioug.github.io/yield-data/saber.json     | [![saber](https://github.com/pioug/yield-data/actions/workflows/saber.yml/badge.svg)](https://github.com/pioug/yield-data/actions/workflows/saber.yml)             |
 | https://pioug.github.io/yield-data/sunny.json     | [![sunny](https://github.com/pioug/yield-data/actions/workflows/sunny.yml/badge.svg)](https://github.com/pioug/yield-data/actions/workflows/sunny.yml)             |
---
+++ quarry.js
@@ -0,0 +1,100 @@
+const fs = require("fs");
+const cheerio = require("cheerio");
+const puppeteer = require("puppeteer");
+const userAgent = require("user-agents");
+
+(async () => {
+  const timestamp = new Date();
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.setUserAgent(userAgent.toString());
+  const rewarders = [
+    "rXhAofQCT7NN9TUqigyEAUzV1uLL4boeD8CRkNBSkYk",
+    "J829VB5Fi7DMoMLK7bsVGFM82cRU61BKtiPz9PNFdL7b",
+    "5LAZ5rUe1CLJoKYauyVXdbG6e7nBmY2j5mJ8PnesCA8z",
+    "3UqiNcE1S2MoQ1M1PvrQf3gCkM7KB1nWfZdoQckobHpz",
+    "3ahWtCwoyv4HHYdDtSzSPzD8DDo6jJKqEmPJrdztFnw4",
+    "9eNj2Y4YdqM9Unw8qTm8kzx1Xt64Rft8a6HjvpkPL7QJ",
+    "CduAhHmZe3n5KkxhP4EjqTZjWTuptYnfXvi4ULyF7fVc",
+    "GuHrjvzqDvLTB27ebd9iFKwceCxKvSswzTByDQUTsvdm",
+  ];
+  const map = new Map();
+  for (const rewarder of rewarders) {
+    const data = await getQuarries(browser, page, rewarder);
+    data.forEach(({ apy, name }) => {
+      map.set(name, apy);
+    });
+  }
+  await browser.close();
+  const data = Array.from(map, ([name, apy]) => ({ name, apy }));
+
+  console.log(data);
+
+  const results = {
+    data,
+    name: "Quarry",
+    timetamp: timestamp.toISOString(),
+    url: "https://app.quarry.so/#/rewarders",
+  };
+  fs.writeFileSync("quarry.json", JSON.stringify(results, null, 2) + "\n");
+})();
+
+async function getQuarries(browser, page, rewarder) {
+  await page.goto(`https://app.quarry.so/#/rewarders/${rewarder}/quarries`, {
+    waitUntil: "networkidle2",
+  });
+  await page.waitForFunction(
+    () => {
+      let total = 0;
+      let loaded = 0;
+      for (const span of document.querySelectorAll("span")) {
+        if (span.textContent.includes("Rewards APY")) {
+          total++;
+        }
+        if ((span.parentNode.nextSibling?.textContent ?? "").includes("%")) {
+          loaded++;
+        }
+      }
+
+      if (
+        window.location.href.includes(
+          "rXhAofQCT7NN9TUqigyEAUzV1uLL4boeD8CRkNBSkYk"
+        )
+      ) {
+        loaded++;
+      }
+
+      return total > 0 && total - loaded <= 1;
+    },
+    {
+      polling: "mutation",
+      timeout: 30000,
+    }
+  );
+  const content = await page.content();
+
+  const $ = cheerio.load(content);
+  const data = $("h1")
+    .filter(function (i, el) {
+      return $(el).text().trim().includes("Rewards");
+    })
+    .parent()
+    .next()
+    .children()
+    .map(function (i, el) {
+      return {
+        name: $(el).find('[size="28"] + div span:nth-child(2)').text().trim(),
+        apy: $(el)
+          .find("span")
+          .filter(function (i, el) {
+            return $(el).text().trim() === "Rewards APY";
+          })
+          .parent()
+          .next()
+          .text(),
+      };
+    })
+    .toArray();
+
+  return data;
+}
