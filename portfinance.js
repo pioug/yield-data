a46a2c4f303dd23e52dc50cf0d692757a521f19c
Gilles Piou <gilles.piou@gmail.com>
2022-05-06 21:57:58 +0800

    Update Port Finance scraper

--- portfinance.js
+++ portfinance.js
@@ -8,13 +8,20 @@ const userAgent = require("user-agents");
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.setUserAgent(userAgent.toString());
-  await page.goto("https://mainnet.port.finance/#/supply", {
-    waitUntil: "networkidle2",
+  await page.setViewport({
+    width: 830,
+    height: 600,
   });
+  await page.goto(
+    "https://mainnet.port.finance/#/lendingMarket/6T4XxKerq744sSuj3jaoV6QiZ8acirf4TrPwQzHAoSy5/markets",
+    {
+      waitUntil: "networkidle2",
+    }
+  );
   await page.waitForFunction(
     () => {
-      for (const em of document.querySelectorAll("em")) {
-        if (em.textContent === "+" && parseFloat(em.nextSibling.textContent)) {
+      for (const td of document.querySelectorAll("td")) {
+        if (td.textContent.includes("%")) {
           return true;
         }
       }
@@ -24,16 +31,22 @@ const userAgent = require("user-agents");
       timeout: 30000,
     }
   );
+  await page.waitForTimeout(5000);
   const content = await page.content();
   await browser.close();
 
   const $ = cheerio.load(content);
   const data = $("tbody")
-    .children()
+    .children(".port-design-table-row")
     .map(function (i, el) {
       return {
-        name: $(el).find("td:first-child span").text().trim(),
-        apy: $(el).find("td:last-child em").text().trim().replace(/\+$/, ''),
+        name: $(el).find("td:first-child span").first().text().trim(),
+        apy: $(el)
+          .find("td:nth-child(3) span")
+          .first()
+          .text()
+          .trim()
+          .replace(/\+$/, ""),
       };
     })
     .toArray();
