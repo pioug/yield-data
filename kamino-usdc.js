846b5f115a37c229e40db7b6696d3f0417fb4bbf
Gilles Piou <gilles.piou@gmail.com>
2024-02-21 01:21:36 +0800

    Add kamino-usdc

---
+++ kamino-usdc.js
@@ -0,0 +1,32 @@
+const fs = require("node:fs");
+const puppeteer = require("puppeteer");
+
+(async () => {
+  const timestamp = new Date();
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.setViewport({ width: 1920, height: 1080 });
+  await page.goto("https://app.kamino.finance/");
+
+  // Find USDC row
+  await page.waitForXPath('//table// *[contains(text(), "USDC")]');
+  const [trElement] = await page.$x(
+    '//table// *[contains(text(), "USDC")]/ancestor::tr'
+  );
+  const [, tvl, , , rate] = await page.evaluate((el) => {
+    const tdElements = el.querySelectorAll("td");
+    return Array.from(tdElements).map((el) => el.textContent.trim());
+  }, trElement);
+
+  await browser.close();
+
+  const results = {
+    id: "kamino-usdc",
+    timestamp: timestamp.toISOString(),
+    protocol: "kamino",
+    name: "USDC",
+    rate: parseFloat(rate),
+    tvl: parseFloat(tvl) * 1000000,
+  };
+  fs.writeFileSync("kamino-usdc.json", JSON.stringify(results) + "\n");
+})();
