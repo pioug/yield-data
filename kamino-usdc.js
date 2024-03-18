4ca3f25d986a4a30713b82071ae1d6114e8be446
Gilles Piou <gilles.piou@gmail.com>
2024-03-18 20:14:12 +0800

    Add new markets of Kamino

--- kamino-usdc.js
+++ kamino-usdc.js
@@ -8,24 +8,32 @@ const puppeteer = require("puppeteer");
   await page.setViewport({ width: 1920, height: 1080 });
   await page.goto("https://app.kamino.finance/");
 
-  // Find USDC row
-  const trElement = await page.waitForSelector(
-    'xpath/(//table// *[contains(text(), "USDC")]/ancestor::tr)[2]',
-  );
-  const [, tvl, , , rate] = await page.evaluate((el) => {
-    const tdElements = el.querySelectorAll("td");
-    return Array.from(tdElements).map((el) => el.textContent.trim());
-  }, trElement);
+  const markets = [
+    ["kamino-usdc-jlp", "USDC (JLP)"],
+    ["kamino-usdc-main", "USDC (Main)"],
+    ["kamino-usdc-alt", "USDC (Alt)"],
+  ];
+
+  for (let i = 0; i < markets.length; i++) {
+    const trElement = await page.waitForSelector(
+      `xpath/(//table// *[text() = "USDC"]/ancestor::tr)[${i + 1}]`,
+    );
+    const [, tvl, , , rate] = await page.evaluate((el) => {
+      const tdElements = el.querySelectorAll("td");
+      return Array.from(tdElements).map((el) => el.textContent.trim());
+    }, trElement);
+
+    const [id, name] = markets[i];
+    const results = {
+      id,
+      timestamp: timestamp.toISOString(),
+      protocol: "kamino",
+      name,
+      rate: parseFloat(rate),
+      tvl: parseFloat(tvl) * 1000000,
+    };
+    fs.writeFileSync(id + ".json", JSON.stringify(results) + "\n");
+  }
 
   await browser.close();
-
-  const results = {
-    id: "kamino-usdc",
-    timestamp: timestamp.toISOString(),
-    protocol: "kamino",
-    name: "USDC",
-    rate: parseFloat(rate),
-    tvl: parseFloat(tvl) * 1000000,
-  };
-  fs.writeFileSync("kamino-usdc.json", JSON.stringify(results) + "\n");
 })();
