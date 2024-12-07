741a9e898ef8984575eee736272c477b3001c3c1
Gilles Piou <gilles.piou@gmail.com>
2024-12-07 20:36:50 +0800

    Fix drift-usdc

--- drift-usdc.js
+++ drift-usdc.js
@@ -6,19 +6,17 @@ const puppeteer = require("puppeteer");
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.setViewport({ width: 1920, height: 1080 });
-  await page.goto("https://app.drift.trade/earn/lend-borrow/deposits");
+  await page.goto("https://app.drift.trade/earn/borrow-lend");
 
   const usdcRow = await page.waitForSelector(
-    'xpath///img[@src="/assets/icons/markets/usdc.svg"]/ancestor::div[1]/parent::*',
+    'xpath///img[@src="https://drift-public.s3.eu-central-1.amazonaws.com/assets/icons/markets/usdc.svg"]/ancestor::div[1]/parent::*',
   );
 
-  const [, , tvl, rate] = await page.evaluate((el) => {
+  const [, tvl, rate] = await page.evaluate((el) => {
     return Array.from(el.children).map((el) => el.textContent.trim());
   }, usdcRow);
 
   await browser.close();
-
-  console.log(rate.split('$').at(-1).replaceAll(".", ""));
 
   const results = {
     id: "drift-usdc",
@@ -26,7 +24,7 @@ const puppeteer = require("puppeteer");
     protocol: "drift",
     name: "USDC",
     rate: parseFloat(rate),
-    tvl: parseFloat(tvl.split('$').at(-1).replaceAll(".", "").replaceAll(",", "") / 100),
+    tvl: parseFloat(tvl.split("$").at(-1).split(",").at(0)) * 1_000_000,
   };
   fs.writeFileSync("drift-usdc.json", JSON.stringify(results) + "\n");
 })();
