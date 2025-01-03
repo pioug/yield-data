cd80d972aebf338cc37c0b14d6fc9f16a8455458
Gilles Piou <gilles.piou@gmail.com>
2025-01-04 02:11:56 +0800

    Add navi-usdc

---
+++ navi-usdc.js
@@ -0,0 +1,34 @@
+const fs = require("node:fs");
+const puppeteer = require("puppeteer");
+
+(async () => {
+  const timestamp = new Date();
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.setViewport({ width: 1920, height: 1080 });
+  await page.goto("https://app.naviprotocol.io/");
+
+  const usdcRow = await page.waitForSelector(
+    'xpath///*[text() = "USDC"]/ancestor::tr',
+  );
+
+  const [, , rate, tvl] = await page.evaluate((el) => {
+    return Array.from(el.querySelectorAll("td")).map((el) =>
+      el.textContent.trim(),
+    );
+  }, usdcRow);
+
+  console.log(rate, tvl);
+
+  await browser.close();
+
+  const results = {
+    id: "navi-usdc",
+    timestamp: timestamp.toISOString(),
+    protocol: "navi",
+    name: "USDC",
+    rate: parseFloat(rate),
+    tvl: parseFloat(tvl.split("$").at(-1).split(",").at(0)) * 1_000_000,
+  };
+  fs.writeFileSync("navi-usdc.json", JSON.stringify(results) + "\n");
+})();
