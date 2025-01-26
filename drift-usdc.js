a4f53d3a332b5842658b081d9569231e44881ac9
Gilles Piou <gilles.piou@gmail.com>
2025-01-26 17:02:05 +0800

    Add drift-usdc-jlp

--- drift-usdc.js
+++ drift-usdc.js
@@ -8,23 +8,29 @@ const puppeteer = require("puppeteer");
   await page.setViewport({ width: 1920, height: 1080 });
   await page.goto("https://app.drift.trade/earn/borrow-lend");
 
-  const usdcRow = await page.waitForSelector(
-    'xpath///img[@src="https://drift-public.s3.eu-central-1.amazonaws.com/assets/icons/markets/usdc.svg"]/ancestor::div[1]/parent::*',
-  );
+  const markets = [
+    ["drift-usdc", "USDC", "usdc.svg"],
+    ["drift-usdc-jlp", "USDC (JLP)", "usdc-1.svg"],
+  ];
 
-  const [, tvl, rate] = await page.evaluate((el) => {
-    return Array.from(el.children).map((el) => el.textContent.trim());
-  }, usdcRow);
+  for (let i = 0; i < markets.length; i++) {
+    const [id, name, image] = markets[i];
+    const usdcRow = await page.waitForSelector(
+      `xpath/(//img[@src="https://drift-public.s3.eu-central-1.amazonaws.com/assets/icons/markets/${image}"]/ancestor::div[1]/parent::*)[1]`,
+    );
+    const [, tvl, rate] = await page.evaluate((el) => {
+      return Array.from(el.children).map((el) => el.textContent.trim());
+    }, usdcRow);
+    const results = {
+      id,
+      timestamp: timestamp.toISOString(),
+      protocol: "drift",
+      name,
+      rate: parseFloat(rate),
+      tvl: parseFloat(tvl) * 1_000_000,
+    };
+    fs.writeFileSync(id + ".json", JSON.stringify(results) + "\n");
+  }
 
   await browser.close();
-
-  const results = {
-    id: "drift-usdc",
-    timestamp: timestamp.toISOString(),
-    protocol: "drift",
-    name: "USDC",
-    rate: parseFloat(rate),
-    tvl: parseFloat(tvl.split("$").at(-1).split(",").at(0)) * 1_000_000,
-  };
-  fs.writeFileSync("drift-usdc.json", JSON.stringify(results) + "\n");
 })();
