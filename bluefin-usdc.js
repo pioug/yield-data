b9690b97c0e014feb367de603fff7d61e33b7e1e
Gilles Piou <gilles.piou@gmail.com>
2026-02-28 23:52:21 +0800

    Fix bluefin-usdc.js

--- bluefin-usdc.js
+++ bluefin-usdc.js
@@ -6,15 +6,17 @@ const puppeteer = require("puppeteer");
   const browser = await puppeteer.launch({ args: ["--no-sandbox"] });
   const page = await browser.newPage();
   await page.setViewport({ width: 1920, height: 1080 });
-  await page.goto("https://trade.bluefin.io/lend");
+  await page.goto("https://trade.bluefin.io/lend", {
+    waitUntil: "networkidle2",
+  });
 
   const trElement = await page.waitForSelector(
-    `xpath/(//tr[.//img[contains(@src, 'usdc-icon')]])`,
+    `xpath/(//tr[td[2][normalize-space() = "USDC"]])`,
   );
   const [, , tvl, , , rate] = await page.evaluate((el) => {
     const tdElements = el.querySelectorAll("td");
     return Array.from(tdElements).map((el) =>
-      el.textContent.trim().match(/[\d,.]+/),
+      el.textContent.replace(/\s+/g, "").trim(),
     );
   }, trElement);
 
@@ -26,7 +28,7 @@ const puppeteer = require("puppeteer");
     protocol: "bluefin",
     name: "USDC",
     rate: parseFloat(rate),
-    tvl: parseFloat(tvl) * 1000000,
+    tvl: parseFloat(tvl.replace(/,/g, "")) * 1_000_000,
   };
 
   console.log(results);
