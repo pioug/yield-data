dacbfafd54ffdb3a7812665919f8fd5ebd8cb2a7
Gilles Piou <gilles.piou@gmail.com>
2026-04-05 10:39:45 +0800

    Fix kamino-usdc.js

--- kamino-usdc.js
+++ kamino-usdc.js
@@ -6,35 +6,32 @@ const puppeteer = require("puppeteer");
   const browser = await puppeteer.launch({ args: ["--no-sandbox"] });
   const page = await browser.newPage();
   await page.setViewport({ width: 1920, height: 1080 });
-  await page.goto("https://kamino.com/markets", {
-    waitUntil: "networkidle0",
+  await page.goto("https://kamino.com/borrow", {
+    waitUntil: "domcontentloaded",
   });
 
-  const markets = [["kamino-usdc-main", "USDC (Main)", 0]];
-
-  for (const market of markets) {
-    const [id, name, index] = market;
-    const trElement = await page.waitForSelector(
-      `xpath/(//table// *[text() = "USDC"]/ancestor::tr)[${index + 1}]`,
-    );
-    const [, tvl, , , rate] = await page.evaluate((el) => {
-      const tdElements = el.querySelectorAll("td");
-      return Array.from(tdElements).map((el) => el.textContent.trim());
-    }, trElement);
-
-    const results = {
-      id,
-      timestamp: timestamp.toISOString(),
-      protocol: "kamino",
-      name,
-      rate: parseFloat(rate),
-      tvl: parseFloat(tvl.replace("$", "")) * 1000000,
-    };
-
-    console.log(results);
-
-    fs.writeFileSync(id + ".json", JSON.stringify(results) + "\n");
-  }
+  const trElement = await page.waitForSelector(
+    'xpath/(//table//tr[td[1][normalize-space() = "USDC"] and td[2][normalize-space() != "$0.00"]])[2]',
+  );
+  const [, tvl, , , rate] = await page.evaluate((el) => {
+    const tdElements = el.querySelectorAll("td");
+    return Array.from(tdElements).map((el) => el.textContent.trim());
+  }, trElement);
 
   await browser.close();
+
+  const [, amount, suffix = ""] = tvl.match(/\$([\d,.]+)([KMB])?/);
+  const results = {
+    id: "kamino-usdc-main",
+    timestamp: timestamp.toISOString(),
+    protocol: "kamino",
+    name: "USDC (Main)",
+    rate: parseFloat(rate),
+    tvl:
+      parseFloat(amount.replaceAll(",", "")) *
+      { "": 1, K: 1e3, M: 1e6, B: 1e9 }[suffix],
+  };
+
+  console.log(results);
+  fs.writeFileSync("kamino-usdc-main.json", JSON.stringify(results) + "\n");
 })();
